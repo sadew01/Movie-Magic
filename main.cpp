3490f3a9c867938f39478fa3975b85ff0b35dd91
RiaChacko2 <92886100+RiaChacko2@users.noreply.github.com>
2022-11-23 11:12:04 -0500

    Updated main.cpp
    I have created Movie.h and Movie.cpp with different helper functions and quick sort. I have an array in the main just to see if my quick sort algorithm works. Feel free to make changes to that.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include <vector>
+#include<fstream>
+#include "Movie.h"
+using namespace std;
+
+
+int main() {
+	Movie obj;
+	int data[] = { 15, 0, 5, 6 };
+	int n = sizeof(data) / sizeof(data[0]);
+	obj.quickSort(data, 0, n - 1);
+	obj.PrintArray(data, n);
+	return 0;
+}
