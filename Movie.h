303809a0fdbdfbd53d27469fe2c4b9d408aefa96
athompson403 <93017097+athompson403@users.noreply.github.com>
2022-11-23 17:05:19 -0500

    Update Movie.h

--- Movie.h
+++ Movie.h
@@ -6,22 +6,20 @@ using namespace std;
 class Movie {
 
 private:
-	string title = "";
-	vector<string> genre = {};
-	int year = 0;
-	int length = 0;
-	float rating = 0.0;
+    string title = "";
+    vector<string> genre = {};
+    int year = 0;
+    int length = 0;
+    float rating = 0.0;
 public:
-	Movie() {
-
-	}
-	string getTitle();
-	vector<string>& getGenre();
-	int getYear();
-	int getLength();
-	float getRating();
-	int partition(int arr[], int low, int high);	// helper functions for Quick Sort
-	void swap(int* a, int* b);
-	void quickSort(int arr[], int low, int high);
-	void PrintArray(int arr[], int size);
+    Movie(string title, vector<string>& genre, int year, int length, float rating);
+    string getTitle();
+    vector<string>& getGenre();
+    int getYear();
+    int getLength();
+    float getRating();
+    int partition(int arr[], int low, int high);	// helper functions for Quick Sort
+    void swap(int* a, int* b);
+    void quickSort(int arr[], int low, int high);
+    void PrintArray(int arr[], int size);
 };
