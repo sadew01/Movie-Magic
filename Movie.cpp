963753afe263b07215e7772b1684ff2a19cc8702
athompson403 <93017097+athompson403@users.noreply.github.com>
2022-11-23 17:04:41 -0500

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -5,62 +5,72 @@
 #include <utility>
 using namespace std;
 
+
+Movie::Movie(string title, vector<string>& genre, int year, int length, float rating) {
+    this->title = title;
+    this->genre = genre;
+    this->year = year;
+    this->length = length;
+    this->rating = rating;
+}
+
 string Movie::getTitle() {
-	return this->title;
+    return this->title;
 }
 vector<string>& Movie::getGenre() {
-	return this->genre;
+    return this->genre;
 }
 int Movie::getYear() {
-	return this->year;
+    return this->year;
 }
 int Movie::getLength() {
-	return this->length;
+    return this->length;
 }
 float Movie::getRating() {
-	return this->rating;
+    return this->rating;
 }
 void Movie::swap(int* a, int* b) { // This code is from the sorting lecture slides
-	int t = *a;
-	*a = *b;
-	*b = t;
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 int Movie::partition(int arr[], int low, int high) { // This code is from the sorting lecture slides
-	int pivot = arr[low];
-	int up = low;
-	int down = high;
-	while (up < down) {
-		for (int j = up; j < high; j++) {
-			if (arr[up] > pivot) {
-				break;
-			}
-			up++;
-		}
-		for (int j = high; j > low; j--) {
-			if (arr[down] < pivot) {
-				break;
-			}
-			down--;
-		}
-		if (up < down) {
-			swap(&arr[up], &arr[down]);
-		}
-	}
-	swap(&arr[low], &arr[down]);
-	return down;
+    int pivot = arr[low];
+    int up = low;
+    int down = high;
+    while (up < down) {
+        for (int j = up; j < high; j++) {
+            if (arr[up] > pivot) {
+                break;
+            }
+            up++;
+        }
+        for (int j = high; j > low; j--) {
+            if (arr[down] < pivot) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            swap(&arr[up], &arr[down]);
+        }
+    }
+    swap(&arr[low], &arr[down]);
+    return down;
 }
 
 void Movie::quickSort(int arr[], int low, int high) { // This code is from the sorting lecture slides
-	if (low < high) {
-		int pivot = partition(arr, low, high);
-		quickSort(arr, low, pivot - 1);
-		quickSort(arr, pivot + 1, high);
-	}
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
 }
 
 void Movie::PrintArray(int arr[], int size) {		// This code is from the sorting lecture slides
-	for (int i = 0; i < size; i++) {
-		cout << arr[i] << " ";
-		
-	}
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+
+    }
 }
+
