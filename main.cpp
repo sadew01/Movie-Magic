e1474d1a6788bbded0be749095529ea286f5b2c5
Sade Whitcomb <93170603+sadew01@users.noreply.github.com>
2022-11-26 14:14:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,4 +13,24 @@ int main() {
 	obj.quickSort(data, 0, n - 1);
 	obj.PrintArray(data, n);
 	return 0;
+	
+	cout << "* Welcome to Movie Magic! *" << endl;
+	cout << "Our goal is to make finding movies suited to your tastes easier." << endl;
+	cout << endl;
+	cout << "Please enter the numbers corresponding to your favorite genres or type in your favorite director." << endl;
+	cout << "(ex. \"1 2 4 6\" or \"Ryan Coogler\")" << endl;
+	cout << "1. Comedy" << endl;
+	cout << "2. Sci-Fi" << endl;
+	cout << "3. Horror" << endl;
+	cout << "4. Romance" << endl;
+	cout << "5. Action" << endl;
+	cout << "6. Thriller" << endl;
+	cout << "7. Drama" << endl;
+	cout << "8. Mystery" << endl;
+	cout << "9. Crime" << endl;
+	cout << "10. Animation" << endl;
+	cout << "11. Adventure" << endl;
+	cout << "12. Fantasy" << endl;
+	cout << "Type 0 to exit." << endl;
+	return 0;
 }
