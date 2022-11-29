1e4fef329c1f67f510e01cc1fd07edf5b8a0e161
Sade Whitcomb <93170603+sadew01@users.noreply.github.com>
2022-11-29 16:30:55 -0500

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -82,3 +82,177 @@ void Movie::PrintArray(int arr[], int size) {		// This code is from the sorting 
     }
 }
 
+bool Movie::validInput(string line) {
+    if (isName(line)) {
+        if (validDirectorName(line)) {
+            return true;
+        }
+    }
+    else if (isNumber(line)) {
+        if (validGenreInput(line)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool Movie::validDirectorName(string director) {
+    for (int i = 0; i < director.length(); i++) {
+        if (isalpha(director.at(i)) != 0 || director.at(i) == ' ') {
+
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Movie::validGenreInput(string line) {
+    istringstream stream(line);
+    vector<int> input;
+    string read;
+
+    // check if the input contains only digits or spaces
+    if (!onlyNumbers(line)) {
+        return false;
+    }
+
+    // read in each number corresponding to a genre
+    while (getline(stream, read, ' ')) {
+        input.push_back(stoi(read));
+    }
+
+    // up to three genres are allowed
+    if (input.size() > 3) {
+        return false;
+    }
+
+    // up to 24 genres
+    for (int i = 0; i < input.size(); i++) {
+        if (input.at(i) <= 0 || input.at(i) > 24) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool Movie::onlyNumbers(string line) {
+    // check that genre input only contains numbers or spaces
+    for (int i = 0; i < line.length(); i++) {
+        if (isdigit(line.at(i)) || line.at(i) == ' ') {
+
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Movie::isName(string line) {
+    // if the first char is a letter, indicates director name input
+    if (isalpha(line.at(0)) != 0) {
+        return true;
+    }
+    return false;
+}
+
+bool Movie::isNumber(string line) {
+    // if the first char is a digit, indicates genre input
+    if (isdigit(line.at(0)) != 0) {
+        return true;
+    }
+    return false;
+}
+
+
+bool Movie::operator<(const Movie& movie1) const {
+    int one = movie1.numRatings;
+    return title < movie1.title;
+}
+
+// radix sort length
+void Movie::radixSortLength(Movie movies[], int size) {
+    int max = findMaxLength(movies, size);
+
+    for (int digit = 1; max / digit > 0; digit *= 10) {
+        radixSortLengthHelper(movies, size, digit);
+    }
+}
+
+void Movie::radixSortLengthHelper(Movie movies[], int size, int digit) {
+
+}
+
+int Movie::findMaxLength(Movie movies[], int size) {
+    int max = movies[0].getLength();
+    for (int i = 0; i < size; i++) {
+        if (movies[i].getLength() > max) {
+            max = movies[i].getLength();
+        }
+    }
+    return max;
+}
+
+// radix sort ratings
+void Movie::radixSortRatings(Movie movies[], int size) {
+    /*
+    int* temp = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        temp[i] = (movies[i] * (float)10);
+    }
+
+    int max = findMaxVal(temp, size);
+
+    for (int digit = 1; max / digit > 0; digit *= 10) {
+        radixSortHelper(temp, size, digit);
+    }
+
+    for (int i = 0; i < size; i++) {
+        movies[i] = ((float)temp[i] / (float)10);
+    }
+    */
+}
+
+void Movie::radixSortRatingsHelper(Movie movies[], int size, int digit) {
+
+}
+
+int Movie::findMaxRating(Movie movies[], int size) {
+    int max = movies[0].getRating();
+    for (int i = 0; i < size; i++) {
+        if (movies[i].getRating() > max) {
+            max = movies[i].getRating();
+        }
+    }
+    return max;
+}
+
+// radix sort numratings
+void Movie::radixSortNumRatings(Movie movies[], int size) {
+    /*
+    int max = findMaxVal(movies, size);
+
+    for (int digit = 1; max / digit > 0; digit *= 10) {
+        radixSortHelper(movies, size, digit);
+    }
+    */
+}
+
+void Movie::radixSortNumRatingsHelper(Movie movies[], int size, int digit) {
+
+}
+
+int Movie::findMaxNumRating(Movie movies[], int size) {
+    int max = movies[0].getNumRatings();
+    for (int i = 0; i < size; i++) {
+        if (movies[i].getNumRatings() > max) {
+            max = movies[i].getNumRatings();
+        }
+    }
+    return max;
+}
