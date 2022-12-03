1f19544975dce224d8b1675d18b17e0f196de909
RiaChacko2 <92886100+RiaChacko2@users.noreply.github.com>
2022-12-03 14:59:49 -0500

    Update Movie.cpp

--- Movie.cpp
+++ Movie.cpp
@@ -69,6 +69,13 @@ void Movie::print() {
 }
 
 
+void Movie::swap(Movie* a, Movie* b) {
+    Movie t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
 int Movie::partition(Movie* arr, int low, int high) { // This code is from the sorting lecture slides
     int pivot = arr[low].getLength();
     int up = low;
@@ -87,15 +94,65 @@ int Movie::partition(Movie* arr, int low, int high) { // This code is from the s
             down--;
         }
         if (up < down) {
-            swap(arr[up], arr[down]);
+            swap(&arr[up], &arr[down]);
         }
     }
-    swap(arr[low], arr[down]);
+    swap(&arr[low], &arr[down]);
+    return down;
+}
+int Movie::partitionRating(Movie* arr, int low, int high) {
+    
+    float pivot = arr[low].getRating();
+    int up = low;
+    int down = high;
+    while (up < down) {
+        for (int j = up; j < high; j++) {
+            if (arr[up].getRating() > pivot) {
+                break;
+            }
+            up++;
+        }
+        for (int j = high; j > low; j--) {
+            if (arr[down].getRating() < pivot) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            swap(&arr[up], &arr[down]);
+        }
+    }
+    swap(&arr[low], &arr[down]);
+    return down;
+}
+
+int Movie::partitionNumOfRatings(Movie* arr, int low, int high) {
+    int pivot = arr[low].getNumRatings();
+    int up = low;
+    int down = high;
+    while (up < down) {
+        for (int j = up; j < high; j++) {
+            if (arr[up].getNumRatings() > pivot) {
+                break;
+            }
+            up++;
+        }
+        for (int j = high; j > low; j--) {
+            if (arr[down].getNumRatings() < pivot) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            swap(&arr[up], &arr[down]);
+        }
+    }
+    swap(&arr[low], &arr[down]);
     return down;
 }
 
 void Movie::quickSort(Movie* arr, int low, int high) { // This code is from the sorting lecture slides
-    
+
     if (low < high) {
         int pivot = partition(arr, low, high);
         quickSort(arr, low, pivot - 1);
@@ -103,14 +160,33 @@ void Movie::quickSort(Movie* arr, int low, int high) { // This code is from the 
     }
 }
 
+void Movie::quickSortRatings(Movie* arr, int low, int high) {
+    if (low < high) {
+        int pivot = partitionRating(arr, low, high);
+        quickSortRatings(arr, low, pivot - 1);
+        quickSortRatings(arr, pivot + 1, high);
+    }
+}
+
+void Movie::quickSortNumOfRatings(Movie* arr, int low, int high) {
+    if (low < high) {
+        int pivot = partitionNumOfRatings(arr, low, high);
+        quickSortNumOfRatings(arr, low, pivot - 1);
+        quickSortNumOfRatings(arr, pivot + 1, high);
+    }
+}
+
 void Movie::PrintArray(Movie* arr, int size) {
     // This code is from the sorting lecture slides
     cout << "Length (from longest): " << endl;
     for (int i = 0; i < size; i++) {
-        cout <<  arr[i].getTitle() << arr[i].getLength() << endl;
+        cout << arr[i].getTitle() << arr[i].getLength() << endl;
 
     }
 }
+
+
+
 
 bool Movie::validInput(string line) {
     if (isName(line)) {
@@ -205,23 +281,23 @@ bool Movie::operator<(const Movie& movie1) const {
 }
 
 void Movie::radixSortLength(Movie* movies, int size) {
-    
+
     int max = findMaxLength(movies, size);
 
-    
+
     for (int digit = 1; max / digit > 0; digit *= 10) {
         radixSortLengthHelper(movies, size, digit);
     }
-    
+
 }
 
 void Movie::radixSortLengthHelper(Movie* movies, int size, int digit) {
-    
-    
+
+
     // create temp array same size as original
     Movie* temp = new Movie[size];
 
-    
+
     // create a counting array to store count of each digit 0-9
     int count[10] = { 0 };
 
@@ -244,9 +320,9 @@ void Movie::radixSortLengthHelper(Movie* movies, int size, int digit) {
     for (int i = 0; i < size; i++) {
         movies[i] = temp[i];
     }
-    
+
     delete[] temp;
-  
+
 }
 
 int Movie::findMaxLength(Movie* movies, int size) {
@@ -272,7 +348,7 @@ void Movie::radixSortRatings(Movie* movies, int size) {
     }
 
     for (int i = 0; i < size; i++) {
-        movies[i].rating = (float) movies[i].rating / (float) 10.0;
+        movies[i].rating = (float)movies[i].rating / (float)10.0;
     }
 }
 
@@ -285,7 +361,7 @@ void Movie::radixSortRatingsHelper(Movie* movies, int size, int digit) {
 
     // store the number of occurences of each character
     for (int i = 0; i < size; i++) {
-        count[((int) movies[i].getRating() / digit) % 10]++;
+        count[((int)movies[i].getRating() / digit) % 10]++;
     }
 
     // update the count array to reflect the position in the final array
@@ -295,8 +371,8 @@ void Movie::radixSortRatingsHelper(Movie* movies, int size, int digit) {
 
     // transfer the values into the temporary array
     for (int i = size - 1; i >= 0; i--) {
-        temp[count[((int) movies[i].getRating() / digit) % 10] - 1] = movies[i];
-        count[((int) movies[i].getRating() / digit) % 10]--;
+        temp[count[((int)movies[i].getRating() / digit) % 10] - 1] = movies[i];
+        count[((int)movies[i].getRating() / digit) % 10]--;
     }
 
     for (int i = 0; i < size; i++) {
@@ -310,8 +386,8 @@ int Movie::findMaxRating(Movie* movies, int size) {
     // finds the max rating
     int max = movies[0].getRating();
     for (int i = 0; i < size; i++) {
-        if ((int) movies[i].getRating() > max) {
-            max = (int) movies[i].getRating();
+        if ((int)movies[i].getRating() > max) {
+            max = (int)movies[i].getRating();
         }
     }
     return max;
